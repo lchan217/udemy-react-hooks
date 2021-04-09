dbba452212b46186011ecbbf578cea9414c72f70
Lizzie <lchan217@gmail.com>
2021-04-08 18:15:24 -0600

    add http req's

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList'
@@ -7,12 +7,41 @@ import Search from './Search';
 const Ingredients = () => {
   const [ userIngredients, setUserIngredients] = useState([])
 
+  useEffect(() => {
+    fetch('https://udemy-react-hooks-c62a0-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = []
+        for(const key in responseData){
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+
+        setUserIngredients(loadedIngredients)
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients)
+  })
+
   const addIngredientHandler = ingredient => {
-    // prevIngredients is like prevState
-    setUserIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ])
+    fetch('https://udemy-react-hooks-c62a0-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json()
+    }).then(responseData => {
+      // prevIngredients is like prevState
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ])
+    })
   }
 
   const removeIngredientHandler = id => {
